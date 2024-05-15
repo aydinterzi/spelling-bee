77c8f4e1dd1714a1471bc5dc6f18e6aae62c772e
Aydin Terzi <aydinterzi7@gmail.com>
2024-05-15 23:43:02 +0300

    dd

--- src/app/word/[locale]/route.ts
+++ src/app/word/[locale]/route.ts
@@ -1,11 +1,11 @@
 export const runtime = "edge";
 
 import { words } from "@/lib/data";
-
+import wordd from "@/lib/words.json";
 export async function GET(
   request: Request,
   { params }: { params: { locale: string } }
 ) {
   const locale = params.locale as keyof typeof words;
-  return Response.json(words[locale]);
+  return Response.json(wordd[locale]);
 }
